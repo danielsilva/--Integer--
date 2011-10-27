21ff6011cf022052fabd4d772e0dd7c0b68a4e84
Daniel Silva <Daniel@Daniel-PC.(none)>
2011-10-27 01:07:00 -0300

    LINQ extensions

---
+++ src/Integer.Infrastructure/LINQExpressions/PredicateBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace Integer.Infrastructure.LINQExpressions
+{
+    public static class PredicateBuilder
+    {
+        public static Expression<Func<T, bool>> True<T>() { return f => true; }
+        public static Expression<Func<T, bool>> False<T>() { return f => false; }
+    }
+}
