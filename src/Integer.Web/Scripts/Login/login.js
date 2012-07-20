a6e34cb54b18244f4ea62259b5b65c25ef867c63
Daniel Silva <Daniel@Daniel-PC.(none)>
2012-07-20 03:49:43 -0300

    login form

---
+++ src/Integer.Web/Scripts/Login/login.js
@@ -0,0 +1,20 @@
+﻿$("#btnlogin").click(function () {
+    var loginform = $("#loginform");
+    loginform.toggle();
+
+    $('body').append("<div id='mask'></div>");
+    $('#mask').css({
+        'position': 'absolute',
+        'left': 0,
+        'right': 0,
+        'top': 0,
+        'bottom': 0,
+        'z-index': 3
+    });
+
+    $('#mask').click(function () {
+        $(this).remove();
+        $("#loginform").hide();
+    });
+});
+
