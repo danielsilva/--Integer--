f5eac67318a141c9472df2cad756798577d65481
Daniel Silva <Daniel@Daniel-PC.(none)>
2012-10-02 09:07:56 -0300

    added existing locals option to event form

---
+++ src/Integer.Web/Controllers/ParoquiaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Integer.Web.Controllers
+{
+    public class ParoquiaController : Controller
+    {
+        [HttpGet]
+        public JsonResult Locals()
+        {
+            // TODO get from database
+            return Json(new List<object> { new { Id = 1, Name = "Templo" }, new { Id = 2, Name = "Salão A" } }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- src/Integer.Web/Scripts/CalendarView/event.js
+++ src/Integer.Web/Scripts/CalendarView/event.js
@@ -1,57 +1,8 @@
 ﻿$().ready(function () {
     configureEventFormModal();
-
-    var reservedLocals = $('#reservedLocals');
-    reservedLocals
-    .mCustomScrollbar({
-        scrollButtons: {
-            enable: true,
-            scrollType: 'pixels',
-            scrollAmount: 80
-        }
-    })
-    .mCustomScrollbar("update");
-
-    $("#btnReserveLocal").click(function () {
-        $('#reservedLocals .mCSB_container').append('<div class="row reserved-local" style="position:relative;"> \
-                    <span title="Remover" class="pull-left removeLocal" aria-hidden="true" data-icon="&#x2612;" style="cursor:pointer; position:absolute; top:50%; font-size:2em; color:rgba(191, 64, 64, 1);"></span> \
-                    <div class="span3"> \
-                        <label>Local</label> \
-                        <select class="span3"> </select> \
-                    </div> \
-                    <div class="span1" style="width:95px;"> \
-                        <label>Dia</label> \
-                        <input type="text" name="reserveDate" class="dateField span1" /> \
-                    </div> \
-                    <div class="span2"> \
-                        <label style="width:100%;font-size:12px;">&nbsp;</label> \
-                        <div class="btn-group clearfix" data-toggle="buttons-checkbox"> \
-                            <button type="button" name="timeMorning" class="btn btn-small">Manhã</button> \
-                            <button type="button" name="timeAfternoon" class="btn btn-small">Tarde</button> \
-                            <button type="button" name="timeEvening" class="btn btn-small">Noite</button> \
-                        </div> \
-                    </div> \
-                </div>')
-                .show('slow');
-        $(".removeLocal").click(function () {
-            $(this).effect("highlight", {}, 3000);
-            $(this).closest("div").remove();
-            configureReservedLocalsScrollBar();
-        });
-        $.each($(".dateField"), function () {
-            $(this).datepicker({
-                prevText: '',
-                nextText: ''
-            });
-        });
-        $('button[name="timeMorning"]').tooltip({ placement: 'top', title: '6h às 12h' });
-        $('button[name="timeAfternoon"]').tooltip({ placement: 'top', title: '12h às 18h' });
-        $('button[name="timeEvening"]').tooltip({ placement: 'top', title: '18h às 22h' });
-
-        configureReservedLocalsScrollBar();
-    });
-
+    configureReservedLocals();
     setDateTimePicker();
+    getExistingLocals();
 });
 
 function setDateTimePicker() {
@@ -81,6 +32,63 @@ function configureEventFormModal() {
     });
 }
 
+function configureReservedLocals() {
+    var reservedLocals = $('#reservedLocals');
+    reservedLocals
+    .mCustomScrollbar({
+        scrollButtons: {
+            enable: true,
+            scrollType: 'pixels',
+            scrollAmount: 80
+        }
+    })
+    .mCustomScrollbar("update");
+
+    var reservedLocalsCount = 0;
+    $("#btnReserveLocal").click(function () {
+        $('#reservedLocals .mCSB_container').append('<div class="row reserved-local" style="position:relative;"> \
+                    <span title="Remover" class="pull-left removeLocal" aria-hidden="true" data-icon="&#x2612;" style="cursor:pointer; position:absolute; top:50%; font-size:2em; color:rgba(191, 64, 64, 1);"></span> \
+                    <div class="span3"> \
+                        <label>Local</label> \
+                        <select name="local[' + reservedLocalsCount + ']" class="span3"> </select> \
+                    </div> \
+                    <div class="span1" style="width:95px;"> \
+                        <label>Dia</label> \
+                        <input type="text" name="reserveDate" class="dateField span1" /> \
+                    </div> \
+                    <div class="span2"> \
+                        <label style="width:100%;font-size:12px;">&nbsp;</label> \
+                        <div class="btn-group clearfix" data-toggle="buttons-checkbox"> \
+                            <button type="button" name="timeMorning" class="btn btn-small">Manhã</button> \
+                            <button type="button" name="timeAfternoon" class="btn btn-small">Tarde</button> \
+                            <button type="button" name="timeEvening" class="btn btn-small">Noite</button> \
+                        </div> \
+                    </div> \
+                </div>')
+                .show('slow');
+
+        $("select[name^='local']").html(existingLocals.join(''));
+
+        $(".removeLocal").click(function () {
+            $(this).effect("highlight", {}, 3000);
+            $(this).closest("div").remove();
+            configureReservedLocalsScrollBar();
+        });
+        $.each($(".dateField"), function () {
+            $(this).datepicker({
+                prevText: '',
+                nextText: ''
+            });
+        });
+        $('button[name="timeMorning"]').tooltip({ placement: 'top', title: '6h às 12h' });
+        $('button[name="timeAfternoon"]').tooltip({ placement: 'top', title: '12h às 18h' });
+        $('button[name="timeEvening"]').tooltip({ placement: 'top', title: '18h às 22h' });
+
+        configureReservedLocalsScrollBar();
+        reservedLocalsCount++;
+    });
+}
+
 function configureReservedLocalsScrollBar() {
     var reservedLocalsCount = $('#reservedLocals .mCSB_container').children().length;
     if (reservedLocalsCount >= 3) {
@@ -91,4 +99,14 @@ function configureReservedLocalsScrollBar() {
     }
     $('#reservedLocals').mCustomScrollbar("update");
     $('#reservedLocals').mCustomScrollbar("scrollTo", "bottom");
+}
+
+var existingLocals = [];
+function getExistingLocals() {
+    $.getJSON("/Paroquia/Locals", function (data) {
+        existingLocals.push('<option value="-1">Selecione</option>');
+        $.each(data, function (index, item) {
+            existingLocals.push('<option value="' + item.Id + '">' + item.Name + '</option>');
+        });
+    });
 }
