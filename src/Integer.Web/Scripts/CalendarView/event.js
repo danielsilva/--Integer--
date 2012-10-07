16f962e7d87220777268563666930e1c4dc2faee
Daniel Silva <Daniel@Daniel-PC.(none)>
2012-10-07 18:56:45 -0300

    form validation

--- src/Integer.Web/Scripts/CalendarView/event.js
+++ src/Integer.Web/Scripts/CalendarView/event.js
@@ -26,9 +26,7 @@ function configureEventFormModal() {
     });
     $('#divFormEvent').on('shown', function (e) {
         var modal = $(this);
-
         modal.css('margin-left', (modal.outerWidth() / 2) * -1);
-
         return this;
     });
 }
@@ -36,54 +34,36 @@ function configureEventFormModal() {
 function configureEventForm() {
     $("#frmEvent").removeAttr("novalidate");
     
-    jQuery.validator.setDefaults({
-        errorElement: "span",
-        errorPlacement: function(error, element) {
-               error.appendTo(element.prev());
-        }
-    });
     $("#frmEvent").validate({
         ignore: [],
         rules: {
             Nome: {
                 required: true
+            },
+            Tipo: {
+                required: true
+            },
+            DataInicio: {
+                required: true
+            },
+            DataFim: {
+                required: true
             }
         },
         submitHandler: function(form) {
-            $('#btnSave').button('loading');​
-            $.post("/Calendario/Salvar", form.serialize())
-                .success(function(){ 
-                    $('#btnSave').button('reset');
-                    $("#msgSuccess").css("visibility", "visible");
-                });
+            if (validateReservedLocalsCount()) {
+                $('#btnSave').button('loading');​
+                $.post("/Calendario/Salvar", $(form).serialize())
+                    .success(function(){ 
+                        $('#btnSave').button('reset');
+                        $("#msgSuccess").css("visibility", "visible");
+                    });
+            }
         }
     });
-
-
-
-//    var validatorSettings = $.data($("#frmEvent")[0], 'validator').settings;
-//    validatorSettings.ignore = [];
-//    var baseErrorPlacement = validatorSettings.errorPlacement;
-//    validatorSettings.errorPlacement = function(error, element) {
-//        baseErrorPlacement(error, element);
-//        //error.appendTo(element.prev());
-//        //offset = element.offset();
-//        error.insert(element.prev())
-//        error.addClass('field-validation-error');  // add a class to the wrapper
-//        //error.css('position', 'absolute');
-//        //error.css('left', offset.left + element.outerWidth());
-//        //error.css('top', offset.top);        
-//    };
-//    var baseSubmitHandler = validatorSettings.submitHandler;
-//    validatorSettings.submitHandler = function(form) {
-//        $('#btnSave').button('loading');​
-//        $.post("/Calendario/Salvar", form.serialize())
-//            .success(function(){ 
-//                $('#btnSave').button('reset');
-//                $("#msgSuccess").css("visibility", "visible");
-//            });
-//            //baseSubmitHanlder(form);
-//    };
+    $("#btnSave").click(function() { 
+        validateReservedLocalsCount();
+    });
 }
 
 function configureReservedLocals() {
@@ -100,28 +80,29 @@ function configureReservedLocals() {
 
     var reservedLocalsCount = 0;
     $("#btnReserveLocal").click(function () {
-        $('#reservedLocals .mCSB_container').append('<div class="row reserved-local" style="position:relative;"> \
+        $('#reservedLocals .mCSB_container')
+            .append('<div class="row reserved-local" style="position:relative;"> \
                     <span title="Remover" class="pull-left removeLocal" aria-hidden="true" data-icon="&#x2612;" style="cursor:pointer; position:absolute; top:50%; font-size:2em; color:rgba(191, 64, 64, 1);"></span> \
                     <div class="span3"> \
-                        <label>Local</label> \
-                        <select name="reservedLocal[' + reservedLocalsCount + '].LocalId" class="span3 localId"> </select> \
+                        <label for="ddlLocal' + reservedLocalsCount + '">Local</label> \
+                        <select id="ddlLocal' + reservedLocalsCount + '" name="reservedLocal[' + reservedLocalsCount + '].LocalId" class="span3 localId"> </select> \
                     </div> \
                     <div class="span1" style="width:95px;"> \
-                        <label>Dia</label> \
-                        <input type="text" name="reservedLocal[' + reservedLocalsCount + '].Date" class="dateField span1 localDate" /> \
+                        <label for="txtDate' + reservedLocalsCount + '">Dia</label> \
+                        <input id="txtDate' + reservedLocalsCount + '" type="text" name="reservedLocal[' + reservedLocalsCount + '].Date" class="dateField span1 localDate" /> \
                     </div> \
                     <div class="span2" style="margin-left:10px;width:130px;"> \
-                        <label style="width:100%;font-size:12px;">&nbsp;</label> \
-                        <input type="hidden" class="timeSelection" value="" /> \
+                        <label for="hddTime' + reservedLocalsCount + '" style="width:100%;font-size:12px;">&nbsp;</label> \
+                        <input id="hddTime' + reservedLocalsCount + '" type="hidden" class="timeSelection" value="" /> \
                         <div class="btn-group clearfix" data-toggle="buttons-checkbox"> \
-                            <button type="button" name="timeMorning" class="btn btn-small">Manhã</button> \
-                            <button type="button" name="timeAfternoon" class="btn btn-small">Tarde</button> \
-                            <button type="button" name="timeEvening" class="btn btn-small">Noite</button> \
+                            <button type="button" id="timeSelectMorning' + reservedLocalsCount + '" class="btn btn-small">Manhã</button> \
+                            <button type="button" id="timeSelectAfternoon' + reservedLocalsCount + '" class="btn btn-small">Tarde</button> \
+                            <button type="button" id="timeSelectEvening' + reservedLocalsCount + '" class="btn btn-small">Noite</button> \
                         </div> \
                     </div> \
                 </div>')
                 .show('slow');
-
+        validateReservedLocalsCount();
         $("select[name='reservedLocal[" + reservedLocalsCount + "].LocalId']").html(existingLocals.join(''));
 
         $(".removeLocal").click(function () {
@@ -135,14 +116,15 @@ function configureReservedLocals() {
                 nextText: ''
             });
         });
-        $('button[name="timeMorning"]').tooltip({ placement: 'top', title: '6h às 12h' });
-        $('button[name="timeAfternoon"]').tooltip({ placement: 'top', title: '12h às 18h' });
-        $('button[name="timeEvening"]').tooltip({ placement: 'top', title: '18h às 22h' });
-        $('button[name^=time]').each(function() {
-            $(this).focusout(function(){ 
-                setTimeReserved($(this));
-            });
-        });
+        $('#timeSelectMorning' + reservedLocalsCount)
+            .tooltip({ placement: 'top', title: '6h às 12h' })
+            .click(function(){ setTimeReserved($(this)); });
+        $('#timeSelectAfternoon' + reservedLocalsCount)
+            .tooltip({ placement: 'top', title: '12h às 18h' })
+            .click(function(){ setTimeReserved($(this)); });
+        $('#timeSelectEvening' + reservedLocalsCount)
+            .tooltip({ placement: 'top', title: '18h às 22h' })
+            .click(function(){ setTimeReserved($(this)); });
 
         configureReservedLocalsValidation();
         configureReservedLocalsScrollBar();
@@ -154,10 +136,7 @@ function configureReservedLocalsValidation()
 {
     $(".localId, .localDate, .timeSelection").each(function() {
         $(this).rules('add', {
-            required: true,
-            messages: {
-                required: "obrigatório"
-            }
+            required: true
         })
     });
 }
@@ -186,4 +165,16 @@ function getExistingLocals() {
 
 function setTimeReserved(element) {
     alert(element.attr("class"));
+}
+
+function validateReservedLocalsCount() { 
+    var reservedLocalsCount = $('#reservedLocals .mCSB_container').children().length;
+    if (reservedLocalsCount > 0) {
+        $('#reservedLocalsError').remove();
+        return true;
+    }
+    else {
+        $('<label id="reservedLocalsError" class="field-validation-error">&nbsp;&nbsp;obrigatório</label>').insertAfter("#btnReserveLocal");
+        return false;
+    }
 }
